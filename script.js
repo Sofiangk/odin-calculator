6e8afbc01d5323390652daf0a0905d507c816fa3
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-09-06 17:30:27 +0200

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(op, n1, n2) {
+  let result = 0;
+  switch (op) {
+    case "+":
+      result = add(n1, n2);
+      break;
+
+    case "-":
+      result = subtract(n1, n2);
+      break;
+
+    case "*":
+      result = multiply(n1, n2);
+      break;
+
+    case "/":
+      result = divide(n1, n2);
+      break;
+
+    default:
+      return "";
+  }
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
